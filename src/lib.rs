46e53ca19af9eea8f5e12748935b1af7f87905b0
Tsumida <elsa110249@163.com>
2020-03-28 13:47:18 +0800

    add merger()

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,24 @@
 use std::collections::{HashMap, BinaryHeap};
 use std::fs::File;
-use std::io::{BufRead, BufReader, Write, BufWriter};
+use std::path::{PathBuf, Path};
+use std::io::{Read, BufRead, BufReader, Write, BufWriter};
 use std::cmp::{Ordering, Ord, Reverse};
 use std::hash::{Hash, Hasher};
 use std::collections::hash_map::DefaultHasher;
 
-
 use serde::{Serialize, Deserialize};
 
-//   Mapper: 
-//  设每条url2KB(IE, 2083char, 如果是ascii --> 2KB)
-//  100GB url ==> 50 M条, 用u32
-//  hasher 承载因子 50%, 每条记录2KB(url) + 24B(String) + 4B(cnt) = 2076B
-//  1K条url需要 2 x 2076 B x 1000 = 4.15MB，
+//  Mapper: 
+//  设每条url2KB(IE最大 2083char, 其他浏览器更长，如果是ascii --> 2KB)
+//  100GB url ==> 50 M条, 用u32记录
+//  每条记录2KB(url) + 24B(String) + 4B(cnt) = 2076B
+//  假定 hashmap load factor 为 50%, 1K条url放入hashmap，需要 2 x 2076 B x 1000 = 4.15MB内存空间
 //  1 GB内存分配 --> 一次最多读入200K条，要830MB, 剩下部分给buffer + 其他--> 250次
-//  这里是否要考虑 HashMap的动态扩容问题？ 
+
+const bf_cap:usize = 100 * (1 << 20); // 100MB
+const hash_cap:usize = 100 * (1 << 20); 
+const topk: usize = 4; // top k
+const MB: usize = 1 << 20;
 
 #[derive(Eq, Debug, Serialize, Deserialize)]
 struct StatEntry{
@@ -34,66 +38,10 @@ impl PartialOrd for StatEntry {
     }
 }
 
-const bf_cap:usize = 100 * (1 << 20); // 100MB
-const hash_cap:usize = 100 * (1 << 20); 
-const topk: usize = 4; // top k
-const MB: usize = 1 << 20;
-
 impl PartialEq for StatEntry {
     fn eq(&self, other: &Self) -> bool {
         self.cnt == other.cnt
     }
-}
-
-// Read elements from temparary files produced by divider and find top-k elements sorted by occurences(desc).
-// assume that there is only one file.
-fn reduce(path: &str){
-    let bf = BufReader::with_capacity(
-        bf_cap, 
-        File::open(path).unwrap()
-    );
-    let mut counter:HashMap<String, u32> = HashMap::with_capacity(hash_cap);
-    for line in bf.lines(){
-        if let Ok(url) = line {
-            *counter.entry(url).or_insert(0u32) += 1;
-        }
-    }
-
-    let mut bheap = BinaryHeap::with_capacity(topk);
-    for (k, v) in counter.iter(){
-        if bheap.len() < topk{
-            bheap.push(Reverse(StatEntry{
-                url: k.clone(),
-                cnt: *v,
-            }));
-        }else if *v > bheap.peek().unwrap().0.cnt{ // 前K个元素最小的一个
-            let _ = bheap.pop();
-            bheap.push(Reverse(StatEntry{
-                url: k.clone(),
-                cnt: *v,
-            }));
-        }
-    }
-
-    let mut cnt = 0;
-    let mut result = Vec::with_capacity(topk);
-    while cnt < topk && !bheap.is_empty(){
-        result.push(bheap.pop().unwrap().0);
-        cnt += 1;
-    }
-    result.reverse();
-    // write result.
-    let s = serde_json::to_string(&result).unwrap();
-    let mut f = File::create("./src/urls/result").unwrap();
-    f.write(s.as_bytes()).unwrap();
-}
-
-// read direct files in the direct.
-fn reducer(dir_path: &str){
-    // read file. 
-    
-
-    
 }
 
 /// Divide the large data into num smaller temparary file, 
@@ -141,10 +89,135 @@ fn divider(num: usize, file_path: &str, mem_mb:usize ){
 
 }
 
+// Read elements from temparary files produced by divider and find top-k elements sorted by occurences(desc).
+// assume that there is only one file.
+fn reduce(path: &Path, target_path: &PathBuf){
+    let bf = BufReader::with_capacity(
+        bf_cap, 
+        File::open(path).unwrap()
+    );
+    let mut counter:HashMap<String, u32> = HashMap::with_capacity(hash_cap);
+    for line in bf.lines(){
+        if let Ok(url) = line {
+            *counter.entry(url).or_insert(0u32) += 1;
+        }
+    }
+
+    let mut bheap = BinaryHeap::with_capacity(topk);
+    for (k, v) in counter.iter(){
+        if bheap.len() < topk{
+            bheap.push(Reverse(StatEntry{
+                url: k.clone(),
+                cnt: *v,
+            }));
+        }else if *v > bheap.peek().unwrap().0.cnt{ // 前K个元素最小的一个
+            let _ = bheap.pop();
+            bheap.push(Reverse(StatEntry{
+                url: k.clone(),
+                cnt: *v,
+            }));
+        }
+    }
+
+    let mut cnt = 0;
+    let mut result = Vec::with_capacity(topk);
+    while cnt < topk && !bheap.is_empty(){
+        result.push(bheap.pop().unwrap().0);
+        cnt += 1;
+    }
+    result.reverse();
+    // write result.
+    let s = serde_json::to_string(&result).unwrap();
+    let mut f = File::create(target_path).unwrap();
+    f.write(s.as_bytes()).unwrap();
+    println!("p={} done,cnt={}", path.display(), cnt);
+}
+
+// read direct files in the given directory.
+fn reducer(dir_path: &str, tmp_path: &str){
+    // read file. 
+    let p = Path::new(dir_path);
+    if !p.is_dir() || !p.exists(){
+        panic!("Error");
+    }
+    
+    let mut tmp_bf = PathBuf::new();
+    tmp_bf.push(tmp_path);
+    if !tmp_bf.exists(){
+        std::fs::create_dir(&tmp_bf).unwrap();
+    }else if !tmp_bf.is_dir(){
+        panic!("Non-dir.");
+    }
+    let mut tmp_index = 0;
+    for entry in p.read_dir().expect("Error occurs while reading directory."){
+        if let Ok(e) = entry{
+            let fp = e.path();
+            if fp.is_file(){
+                tmp_bf.push(tmp_index.to_string());
+                println!("{:?}", e.path());
+                reduce(&fp, &tmp_bf);
+                tmp_bf.pop();
+            }
+            tmp_index += 1;
+        }
+    }
+    
+}
+
+fn merge(fp: &PathBuf, bheap:&mut BinaryHeap<Reverse<StatEntry>>, ){
+    println!("merging {:?}",fp);
+    let mut fr = File::open(fp).unwrap(); 
+    let mut sbuf = String::with_capacity(100 * MB);
+    fr.read_to_string(&mut sbuf).unwrap();
+    let s: Vec<StatEntry> = serde_json::from_str(&sbuf).unwrap();
+    for ste in s{
+        if bheap.len() < topk{
+        bheap.push(Reverse(ste));
+        }else if ste.cnt > bheap.peek().unwrap().0.cnt{ // 前K个元素最小的一个
+            let _ = bheap.pop();
+            bheap.push(Reverse(ste));
+        }
+    }
+}
+
+// find top-k elements from temp files produced by reducer.
+fn merger(dir_path: &str, res_path: &str){
+    let p = Path::new(dir_path);
+    if !p.is_dir() || !p.exists(){
+        panic!("Error");
+    }
+    let mut bheap = BinaryHeap::with_capacity(topk);
+    p.read_dir()
+    .expect("Error occurs while reading directory.")
+    .for_each(|entry|{
+        if let Ok(e) = entry{
+            let fp = e.path();
+            if fp.is_file(){
+                merge(&fp, &mut bheap);
+            }
+        }
+    });
+    let mut result = Vec::with_capacity(topk);
+    while let Some(ste) = bheap.pop(){
+        result.push(ste.0);
+    }
+    result.reverse();
+    for ste in &result{
+        println!("cnt={}, url={}", ste.cnt, ste.url);
+    }
+    let mut resf = BufWriter::with_capacity(10 * MB, File::create(res_path).unwrap());
+    for str in result{
+        resf.write(
+            serde_json::to_string(&str).unwrap().as_bytes()
+        ).unwrap();
+    }
+}
+
+/// Remember append '\n' to each url.
 fn gen_case(){
     //let max_size = 1024; // url长度在1024Byte以内;
     //let k = 20; // top k 个url
-    let at_least = 20000;
+    let at_least = 50000;
     let topk_urls = vec![
         "https://rust-random.github.io/book/guide-seq.html\n",
         "https://lib.rs/crates/rand\n",
@@ -156,6 +229,14 @@ fn gen_case(){
         "https://www.gamersky.com/news/202003/1275181.shtml\n",
         "http://i.gamersky.com/u/2945817/\n",
         "https://voice.baidu.com/act/newpneumonia/newpneumonia/?from=osari_pc_1\n",
+        "https://doc.rust-lang.org/std/io/trait.BufRead.html#method.read_line\n",
+        "https://doc.rust-lang.org/std/io/trait.BufRead.html#tymethod.consume\n",
+        "https://doc.rust-lang.org/std/io/trait.BufRead.html#method.read_until\n",
+        "https://www.gamersky.com/z/lostplanet2/\n",
+        "https://www.gamersky.com/news/202003/1275413.shtml\n",
+        "https://down.gamersky.com/pc/201309/296720.shtml\n",
+        "https://www.baidu.com/link?url=9j5tYoOerbZ9DLin7lNnJlk94g0C41xAYOPayV4tA0yBBGpRWN1yCPA-OuAnFANAXLpCsD0WJaoQEQ1jMQyK4q&wd=&eqid=b4bec90a00007e94000000065e7ee3c2\n",
+        "https://blog.csdn.net/canot/article/details/53966987\n",
     ].into_iter().map(|s| s.as_bytes()).collect::<Vec<&[u8]>>();
 
     let size = topk_urls.len();
@@ -170,33 +251,67 @@ fn gen_case(){
     println!("total: {} Bytes\n", bytes_cnt);
 }
 
-#[test]
-fn test_reducer() {
-    reducer("./src/urls");
-}
+#[cfg(test)]
+mod test_topk{
+    use super::*;
 
-#[test]
-#[ignore]
-fn test_gen_case() {
-    gen_case();
-}
+    #[test]
+    #[ignore]
+    fn test_gen_case() {
+        gen_case();
+    }
 
-#[test]
-fn test_divider() {
-    let num = 31;
-    let p = "./src/urls/input.txt";
-    divider(num, p, 800);
-}
+    #[test]
+    #[ignore]
+    fn test_divider() {
+        let num = 31;
+        let p = "./src/urls/input.txt";
+        divider(num, p, 800);
+    }
 
-#[test]
-fn test_hasher(){
-    let mut hasher = DefaultHasher::new();
-    "abc".hash(&mut hasher);
-    let h1 = hasher.finish(); // hash state has changed.
-    "abc".hash(&mut hasher);
-    let h2 = hasher.finish();
-    assert_ne!(
-        h1,
-        h2,
-    );
+    #[test]
+    #[ignore]
+    fn test_reducer() {
+        let divided_path = "./src/tmps";
+        let tmp_dir_path = "./src/reduced";
+        reducer(divided_path, tmp_dir_path);
+    }
+
+    #[test]
+    #[ignore]
+    fn test_merger() {
+        let tmp_dir_path = "./src/reduced";
+        let res_path = "./src/result";
+        merger(tmp_dir_path, res_path);
+    }
+
+    #[test]
+    #[ignore]
+    fn test_hasher(){
+        let mut hasher = DefaultHasher::new();
+        "abc".hash(&mut hasher);
+        let h1 = hasher.finish(); // hash state has changed.
+        "abc".hash(&mut hasher);
+        let h2 = hasher.finish();
+        assert_ne!(
+            h1,
+            h2,
+        );
+    }
+
+    #[test]
+    fn test_proc() {
+        gen_case();
+
+        let num = 31;
+        let p = "./src/urls/input.txt";
+        divider(num, p, 800);
+
+        let divided_path = "./src/tmps";
+        let tmp_dir_path = "./src/reduced";
+        reducer(divided_path, tmp_dir_path);
+
+        let res_path = "./src/result";
+        merger(tmp_dir_path, res_path);
+    }
 }
